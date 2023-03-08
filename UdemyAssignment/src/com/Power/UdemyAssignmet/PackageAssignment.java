97544fbac7e2b53dca6ea9bf8a791b0410d23624
Gauri Ragade <gauriragade98@gmail.com>
2023-03-08 17:11:40 +0530

    Package Assignment

---
+++ UdemyAssignment/src/com/Power/UdemyAssignmet/PackageAssignment.java
@@ -0,0 +1,8 @@
+package com.Power.UdemyAssignmet;
+import static java.lang.System.out;
+public class PackageAssignment {
+	public static void main(String[] args) {
+		Math.sqrt(36);
+		out.println(Math.sqrt(36));
+	}
+}
