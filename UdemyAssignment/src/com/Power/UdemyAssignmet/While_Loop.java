1fde73ccf3faed5b9f2bdfc8f904b14a04d8c827
Gauri Ragade <gauriragade98@gmail.com>
2023-03-08 17:01:17 +0530

    While loop logic

---
+++ UdemyAssignment/src/com/Power/UdemyAssignmet/While_Loop.java
@@ -0,0 +1,16 @@
+package com.Power.UdemyAssignmet;
+
+public class While_Loop {
+	public static void main(String[] args) {
+		int i = 1;
+		int n = 1;
+		while (i <= 10) {
+
+			int ans = 2 * i;
+			System.out.println("2*"+ i+"=" + ans);
+			i++;
+
+		}
+	}
+	// System.out.println("3*"+i);
+}
