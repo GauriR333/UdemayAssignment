8a804ed3874107699ac63da930d5bbff0fa52525
Gauri Ragade <gauriragade98@gmail.com>
2023-03-08 17:16:52 +0530

    Fist assignment on udemy using prime number

---
+++ UdemyAssignment/src/com/Power/UdemyAssignmet/FirstAssignment.java
@@ -0,0 +1,15 @@
+package com.Power.UdemyAssignmet;
+
+public class FirstAssignment {
+	public static void main(String args[]) {
+		int a = 10;
+		double b = 20.25;
+		char c = 'X';
+		String s = "i am the best";
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(s);
+	}
+
+}
